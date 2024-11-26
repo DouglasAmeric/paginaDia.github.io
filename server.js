d4d6f510c1659594a22621390ba5498b070fd93a
Douglas A. <34975337+DouglasAmeric@users.noreply.github.com>
2024-11-26 20:36:32 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ http.createServer((req,res)=>{
     });
   }
   //pagina HOME
-  else if(req.url === '/home/homePG.html'){
+  else if(req.url === '/home/'){
     const filePhat = path.join(__dirname,'home','homePG.html')
     fs.readFile(filePhat, (err,data)=>{
       if(err){
@@ -30,6 +30,31 @@ http.createServer((req,res)=>{
         res.end(data)
       }
     });
+  }
+  // pagina FAQ
+  else if(req.url === '/faq/'){
+    const filePhat = path.join(__dirname,'faq','faq.html')
+    fs.readFile(filePhat,(err,data)=>{
+      if(err){
+        res.writeHead(404)
+        res.end("arquivo não encontrado :(")
+      }else{
+        res.writeHead(200)
+        res.end(data)
+      }
+    })
+  }
+  else if(req.url === '/sobre/'){
+    const filePhat = path.join(__dirname, 'sobre','sobre.html')
+    fs.readFile(filePhat,(err,data)=>{
+      if(err){
+        res.writeHead(404)
+        res.end('arquivo não encontrado :(')
+      }else{
+        res.writeHead(200)
+        res.end(data)
+      }
+    })
   }
   // carregando o  CSS das Paginas
   else if(req.url.endsWith('.css')){
@@ -44,42 +69,41 @@ http.createServer((req,res)=>{
       }
     });
   }
-  // criando uma URL para as imagens
-  else if(req.url === '/manha'){
-    const phatImage = path.join(__dirname,'/home/imgs/manha.png')
-    fs.readFile(phatImage,(err,data)=>{
-      if(err){
+  else if(req.url === '/dia'){
+    const phatImage = path.join(__dirname,'home','imgs','manha.png')
+    fs.readFile(phatImage,(erro,data)=>{
+      if(erro){
         res.writeHead(404)
-        res.end("Erro ao acessar as imagens")
+        res.end("imagen não encontrada :(")
       }else{
         res.writeHead(200)
-        res.end(data) 
+        res.end(data)
       }
-    });
+    })
   }
   else if(req.url === '/tarde'){
-    const phatImage = path.join(__dirname,'/home/imgs/tarde.png')
-    fs.readFile(phatImage,(err,data)=>{
-      if(err){
+    const phatImage = path.join(__dirname,'home','imgs','tarde.png')
+    fs.readFile(phatImage,(erro,data)=>{
+      if(erro){
         res.writeHead(404)
-        res.end("Erro ao acessar as imagens")
+        res.end("imagen não encontrada :(")
       }else{
         res.writeHead(200)
-        res.end(data) 
+        res.end(data)
       }
-    });
+    })
   }
   else if(req.url === '/noite'){
-    const phatImage = path.join(__dirname,'/home/imgs/noite.png')
-    fs.readFile(phatImage,(err,data)=>{
-      if(err){
+    const phatImage = path.join(__dirname,'home','imgs','noite.png')
+    fs.readFile(phatImage,(erro,data)=>{
+      if(erro){
         res.writeHead(404)
-        res.end("Erro ao acessar as imagens")
+        res.end("imagen não encontrada :(")
       }else{
         res.writeHead(200)
-        res.end(data) 
+        res.end(data)
       }
-    });
+    })
   }
   else{
     res.writeHead(404)
